938fbec6d934b83c255bcb551d89f05c8314de56
Paul Llanque <llanquepol@gmail.com>
2023-05-06 20:52:52 -0500

    ADD: list todos

---
+++ todos.rb
@@ -0,0 +1,9 @@
+# data
+id = 0
+todos = [
+  { id: (id = id.next), content: 'Fill the weekly feedback', completed: false },
+  { id: (id = id.next), content: 'Complete Ruby Basics 1', completed: false },
+  { id: (id = id.next), content: 'Complete Ruby Basics 2', completed: false },
+  { id: (id = id.next), content: 'Complete Ruby Methods', completed: false },
+  { id: (id = id.next), content: 'Do meditation', completed: false }
+]
