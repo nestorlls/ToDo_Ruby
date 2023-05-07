a21c475d61768cd719bb95a6d3c2b752d7838403
Paul Llanque <llanquepol@gmail.com>
2023-05-06 20:58:00 -0500

    ADD: show list of list

--- todos.rb
+++ todos.rb
@@ -7,3 +7,18 @@ todos = [
   { id: (id = id.next), content: 'Complete Ruby Methods', completed: false },
   { id: (id = id.next), content: 'Do meditation', completed: false }
 ]
+
+# methods
+def welcome
+  puts "#{'-' * 24} Welcome to ToDos #{'-' * 24} \n\n"
+end
+
+def list_todos(todos)
+  todos.each do |todo|
+    puts "#{todo[:id]}. #{todo[:content]}"
+  end
+end
+
+# main
+welcome()
+list_todos(todos)
